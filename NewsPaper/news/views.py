cb398a517387e63e75e024ffc136c60c6afaa7f2
Александр <96573790+SHURIK89@users.noreply.github.com>
2023-02-02 17:09:53 +0300

    Update views.py

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,13 +1,3 @@
 from django.shortcuts import render
 
-class NewsList(ListView):
-    model = Post
-    context_object_name = 'news'
-    template_name = 'news.html'
-
-
-class NewsDetail(DetailView):
-    model = Post
-    template_name = 'article.html'
-    context_object_name = 'article'
 
